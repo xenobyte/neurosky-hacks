6fcfbe500ac8e052d03334da9b5d16492089c735
xenobyte <timo.brd@gmx.net>
2014-09-14 09:24:04 +0200

    Initial commit

---
+++ usermeanalphabeta.m
@@ -0,0 +1,12 @@
+fnm = cellstr(ls('*.csv'));
+for i = 1:length(fnm)
+    raweeg = csvread(fnm{i},2560,1); % read 5 seconds from csv
+    alpha = filter(alphafilter, raweeg); % filter alpha (8-13 Hz)
+    beta = filter(betafilter, raweeg); % filter beta (13-30 Hz)
+    medianalpha = median(alpha); % calculate median for alpha
+    medianbeta = median(beta); % calculate median for beta
+    Y = [medianalpha medianbeta]; % save results for this iteration
+    X = [X Y]; % append this iteration to the other results
+    disp('Datensatz fertig')
+end
+save('cal.mat', 'X')
